cf75b33be07439ef43dec6552317f88a64c298ba
EmanuelCN <emanuelghub@gmail.com>
2022-06-16 00:44:11 +0300

    devfreq: Inline with msm-5.4
    Change-Id: Ic2cbbd325c4624bfef24f7a89d56ff08b9bba846

--- drivers/devfreq/arm-memlat-mon.c
+++ drivers/devfreq/arm-memlat-mon.c
@@ -28,6 +28,7 @@
 #include <linux/mutex.h>
 #include <linux/cpu.h>
 #include <linux/spinlock.h>
+#include <soc/qcom/scm.h>
 
 enum common_ev_idx {
 	INST_IDX,
@@ -339,7 +340,7 @@ static int init_common_evs(struct memlat_cpu_grp *cpu_grp,
 		for (i = 0; i < NUM_COMMON_EVS; i++) {
 			ret = set_event(&common_evs[i], cpu,
 					cpu_grp->common_ev_ids[i], attr);
-			if (ret)
+			if (ret < 0)
 				break;
 		}
 	}
@@ -382,7 +383,7 @@ static void memlat_monitor_work(struct work_struct *work)
 		df = mon->hw.df;
 		mutex_lock(&df->lock);
 		err = update_devfreq(df);
-		if (err)
+		if (err < 0)
 			dev_err(mon->hw.dev, "Memlat update failed: %d\n", err);
 		mutex_unlock(&df->lock);
 	}
@@ -606,7 +607,7 @@ static int start_hwmon(struct memlat_hwmon *hw)
 		hp_idle_register_cnt++;
 		mutex_unlock(&notify_lock);
 		ret = init_common_evs(cpu_grp, attr);
-		if (ret)
+		if (ret < 0)
 			goto unlock_out;
 
 		INIT_DEFERRABLE_WORK(&cpu_grp->work, &memlat_monitor_work);
@@ -620,7 +621,7 @@ static int start_hwmon(struct memlat_hwmon *hw)
 				continue;
 			ret = set_event(&mon->miss_ev[idx], cpu,
 					mon->miss_ev_id, attr);
-			if (ret)
+			if (ret < 0)
 				goto unlock_out;
 
 			if (mon->access_ev_id && mon->wb_ev_id) {
@@ -671,8 +672,13 @@ static void stop_hwmon(struct memlat_hwmon *hw)
 		unsigned int idx = cpu - cpumask_first(&mon->cpus);
 		struct dev_stats *devstats = to_devstats(mon, cpu);
 
-		if (mon->miss_ev)
+		if (mon->miss_ev) {
 			delete_event(&mon->miss_ev[idx]);
+			if (mon->wb_ev)
+				delete_event(&mon->wb_ev[idx]);
+			if (mon->access_ev)
+				delete_event(&mon->access_ev[idx]);
+		}
 		devstats->inst_count = 0;
 		devstats->mem_count = 0;
 		devstats->freq = 0;
@@ -829,7 +835,7 @@ static int memlat_cpu_grp_probe(struct platform_device *pdev)
 		return -ENOMEM;
 
 	ret = of_property_read_u32(dev->of_node, "qcom,inst-ev", &event_id);
-	if (ret) {
+	if (ret < 0) {
 		dev_dbg(dev, "Inst event not specified. Using def:0x%x\n",
 			INST_EV);
 		event_id = INST_EV;
@@ -837,7 +843,7 @@ static int memlat_cpu_grp_probe(struct platform_device *pdev)
 	cpu_grp->common_ev_ids[INST_IDX] = event_id;
 
 	ret = of_property_read_u32(dev->of_node, "qcom,cyc-ev", &event_id);
-	if (ret) {
+	if (ret < 0) {
 		dev_dbg(dev, "Cyc event not specified. Using def:0x%x\n",
 			CYC_EV);
 		event_id = CYC_EV;
@@ -845,7 +851,7 @@ static int memlat_cpu_grp_probe(struct platform_device *pdev)
 	cpu_grp->common_ev_ids[CYC_IDX] = event_id;
 
 	ret = of_property_read_u32(dev->of_node, "qcom,stall-ev", &event_id);
-	if (ret)
+	if (ret < 0)
 		dev_dbg(dev, "Stall event not specified. Skipping.\n");
 	else
 		cpu_grp->common_ev_ids[STALL_IDX] = event_id;
@@ -908,7 +914,8 @@ static int memlat_mon_probe(struct platform_device *pdev, bool is_compute)
 		if (!cpumask_subset(&mon->cpus, &cpu_grp->cpus)) {
 			dev_err(dev,
 				"Mon CPUs must be a subset of cpu_grp CPUs. mon=%*pbl cpu_grp=%*pbl\n",
-				mon->cpus, cpu_grp->cpus);
+				cpumask_pr_args(&mon->cpus),
+				cpumask_pr_args(&cpu_grp->cpus));
 			ret = -EINVAL;
 			goto unlock_out;
 		}
@@ -962,7 +969,7 @@ static int memlat_mon_probe(struct platform_device *pdev, bool is_compute)
 
 		ret = of_property_read_u32(dev->of_node, "qcom,cachemiss-ev",
 						&event_id);
-		if (ret) {
+		if (ret < 0) {
 			dev_err(dev, "Cache miss event missing for mon: %d\n",
 					ret);
 			ret = -EINVAL;
@@ -1044,7 +1051,7 @@ static int arm_memlat_mon_driver_probe(struct platform_device *pdev)
 		return -EINVAL;
 	}
 
-	if (ret) {
+	if (ret < 0) {
 		dev_err(dev, "Failure to probe memlat device: %d\n", ret);
 		return ret;
 	}
