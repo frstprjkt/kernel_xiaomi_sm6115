8b705d1b87375dd19052a33aad40925d688cf56a
Vayruz Rafli <vayruzrafli79@outlook.co.id>
2023-03-09 23:42:09 +0700

    [ci skip] build: Update build script
    Change-Id: I3ee45ca30f6804d57541fb9b21523bf88f017d2d

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ export CODENAME="SM6115"
 export DEVICE="Xiaomi SM6115 platform"
 
 # Source information
-export BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+export BRANCH_NAME="$(git rev-parse --abbrev-ref HEAD)"
 export LAST_COMMIT="$(git log --pretty=format:'%h: %s' -1)"
 
 # Function environment
@@ -44,7 +44,7 @@ if [ -a "$(pwd)/gcc*" ]; then
 fi
 
 # Notify kernel compilation started
-post_msg "<b>Katana Kernel Compilation Started</b>%0Aby the <b>frostedscape.</b> org%0A%0A<b>Host OS: </b><code>$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')</code>%0A<b>Kernel version: </b><code>$(make kernelversion | grep 4.19)</code>%0A<b>Build date: </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device: </b><code>$DEVICE [$CODENAME]</code>%0A<b>Hostname: </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host core count: </b><code>$(nproc --all)</code>%0A<b>Compiler used: </b><code>$($(pwd)/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')</code>%0A<b>Branch: </b><code>$BRANCH</code>%0A<b>Last commit: </b><code>$LAST_COMMIT</code>%0A%0A#katana #$CODENAME"
+post_msg "<b>Katana Kernel Compilation Started</b>%0Aby the <b>frostedscape.</b> org%0A%0A<b>Host OS: </b><code>$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')</code>%0A<b>Kernel version: </b><code>$(make kernelversion | grep 4.19)</code>%0A<b>Build date: </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device: </b><code>$DEVICE [$CODENAME]</code>%0A<b>Hostname: </b><code>$KBUILD_BUILD_HOST</code>%0A<b>Host core count: </b><code>$(nproc --all)</code>%0A<b>Compiler used: </b><code>$($(pwd)/clang/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')</code>%0A<b>Branch: </b><code>$BRANCH_NAME</code>%0A<b>Last commit: </b><code>$LAST_COMMIT</code>%0A%0A#katana #$CODENAME"
 
 # Cleanup before compiling
 rm -f AnyKernel3/Image
@@ -77,7 +77,7 @@ mv $IMAGE AnyKernel3
 
 # Compress AnyKernel3 into zip
 cd AnyKernel3
-zip -r9 katana-$BRANCH-$CODENAME-$(date +%d%H%M).zip .
+zip -r9 katana-$BRANCH_NAME-$CODENAME-$(date +%d%H%M).zip .
 cd ..
 
 # Upload kernel zip
